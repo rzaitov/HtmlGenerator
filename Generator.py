780b22a29b5a7a4f4f8fe8c09ebfd6ff9d4eb653
Rustam Zaitov <rustam.zaitov@xamarin.com>
2014-09-28 18:30:05 +0400

    output dir inject as constructor parameter. Also unsure that output dir exists

--- Generator.py
+++ Generator.py
@@ -1,6 +1,6 @@
 import os
 class Generator:
-	def __init__(self, templatePath, xlsHelper, columnNames, fileNameColumn):
+	def __init__(self, templatePath, xlsHelper, columnNames, fileNameColumn, output_dir="output"):
 		assert templatePath is not None
 		assert xlsHelper is not None
 		assert columnNames is not None
@@ -10,13 +10,19 @@ class Generator:
 		self.xlsHelper = xlsHelper
 		self.columnNames = columnNames
 		self.fileNameColumn = fileNameColumn
+		self.output_dir = output_dir
+
+		# ensure that self.output_dir exists
+		if not os.path.exists(self.output_dir):
+			os.makedirs(self.output_dir)
 
 		f = open(self.templatePath)
 		self.template = f.read().decode('windows-1251')
 
 	def GenerateFor(self, rowIndex):
 		fileName = self.xlsHelper.GetValue(rowIndex, self.fileNameColumn)
-		filePath = os.path.join('output', fileName)
+
+		filePath = os.path.join(self.output_dir, fileName)
 		paramMap = self.FetchMapFor(rowIndex)
 
 		content = self.template % paramMap
